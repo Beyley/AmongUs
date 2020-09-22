5c06f27bce331e5917f9d2aad8a0783cc81b32ce
PoltixeTheDerg <34501060+PoltixeTheDerg@users.noreply.github.com>
2020-09-21 19:07:47 -0700

    Begin adding functionality to the callmeeting command

--- AmongUs/src/main/java/poltixe/spigot/amongus/CommandCallMeeting.java
+++ AmongUs/src/main/java/poltixe/spigot/amongus/CommandCallMeeting.java
@@ -3,11 +3,28 @@ package poltixe.spigot.amongus;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class CommandCallMeeting implements CommandExecutor {
+    App app = App.getPlugin(App.class);
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        PlayerState senderState = null;
+
+        for (PlayerState state : app.playerStates) {
+            if (state.player.getName() == sender.getName()) {
+                senderState = state;
+            }
+        }
+
+        if (senderState.meetingsLeft == 0) {
+            senderState.player.sendMessage(ChatColor.RED + "You have no emergency meetings left!");
+            return true;
+        }
+
         return true;
     }
 }
